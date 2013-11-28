0c1622db28678500f6eebc35bc1a5fc8d9595766
Satu Peltola <satu.peltola@aalto.fi>
2013-11-28 11:28:27 +0100

    Creating

---
+++ LectureApp/LectureApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LectureApp
+//
+//  Created by Satu Peltola on 28/11/13.
+//  Copyright (c) 2013 Satu Peltola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LectureApp/LectureAppTests/LectureAppTests.m
@@ -0,0 +1,34 @@
+//
+//  LectureAppTests.m
+//  LectureAppTests
+//
+//  Created by Satu Peltola on 28/11/13.
+//  Copyright (c) 2013 Satu Peltola. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LectureAppTests : XCTestCase
+
+@end
+
+@implementation LectureAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
