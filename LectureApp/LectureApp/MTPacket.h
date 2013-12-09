fff5404c9e3b3759147633c205c6d2ed07ccc015
Satu Peltola <satu.peltola@aalto.fi>
2013-12-09 11:10:29 +0100

    MTPacket class + sending packets

---
+++ LectureApp/LectureApp/MTPacket.h
@@ -0,0 +1,28 @@
+//
+//  MTPacket.h
+//  LectureApp
+//
+//  Created by Satu Peltola on 09/12/13.
+//  Copyright (c) 2013 Satu Peltola. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const MTPacketKeyData;
+extern NSString * const MTPacketKeyType;
+extern NSString * const MTPacketKeyAction;
+
+typedef enum { MTPacketTypeUnknown = -1 } MTPacketType;
+typedef enum { MTPacketActionUnknown = -1 } MTPacketAction;
+
+@interface MTPacket : NSObject
+
+@property (strong, nonatomic) id data;
+@property (assign, nonatomic) MTPacketType type;
+@property (assign, nonatomic) MTPacketAction action;
+
+#pragma mark -
+#pragma mark Initialization
+- (id)initWithData:(id)data type:(MTPacketType)type action:(MTPacketAction)action;
+
+@end
